5835c920f266f541b991f28e2ab38f34d911791b
059016 <hust930917@gmail.com>
2024-05-16 18:58:21 +0800

    Add files via upload

---
+++ to student/js/script.js
@@ -0,0 +1,49 @@
+let menu=document.querySelector("#menu-bar");
+let navbar=navbar=document.querySelector(".navbar");
+
+menu.onclick=()=>{
+	menu.classList.toggle('fa-times');
+	navbar.classList.toggle('active');
+}
+
+Window.onscroll=()=>{
+	if (Window.scrollY>60){
+		
+document.querySelector('#scroll-up').classList.add('active');}
+	else{
+			
+document.querySelector('#scroll-up').classList.remove('active');}
+}
+const scriptURL='部屬後的網址';
+const form=document.forms[''orderlist];
+
+form.addEventListener('submit' ,e=>{
+	e.preventDefault()
+})
+
+
+// 按下按鈕之後產生的變化
+menu.onclick = () => {
+    
+	
+}
+
+// 回到上層按鈕
+window.onscroll = () => {
+    if (window.scrollY > 60) {
+        
+
+    } else {
+        
+    }
+}
+
+const scriptURL='';
+const form = document.forms[''];
+	  
+form.addEventListener('submit', e => {
+	
+	
+	
+	
+})
